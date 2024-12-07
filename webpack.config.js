40fd6c6221de1a9efd0a21600d8eecfd26ce3a37
allan-viveiros <viveirosallan@gmail.com>
2024-12-07 15:12:26 -0500

    tweaks for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const path = require("path");
 module.exports = {
   entry: './src/index.js',  
   output: { path: path.resolve(__dirname, 'dist'), 
-  filename: 'bundle.js', publicPath: '/' 
+  filename: 'bundle.js', 
+  publicPath: '/' 
   },
   module: {
     rules: [
